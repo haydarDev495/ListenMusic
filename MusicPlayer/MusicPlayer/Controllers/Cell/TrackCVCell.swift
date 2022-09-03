21d0b1845514a4ff3a71293a74ec69c8032fcc85
Haydar Bekmuradov <haydarbekmuradov50@gmail.com>
2022-09-03 12:46:43 +0300

    added track's

---
+++ MusicPlayer/MusicPlayer/Controllers/Cell/TrackCVCell.swift
@@ -0,0 +1,13 @@
+//
+//  TrackCVCell.swift
+//  MusicPlayer
+//
+//  Created by Haydar Bekmuradov on 3.09.22.
+//
+
+import UIKit
+
+class TrackCVCell: UICollectionViewCell {
+    @IBOutlet weak var musicImage: UIImageView!
+
+}
